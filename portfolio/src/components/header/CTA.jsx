4a6d4751e237c580d9d3d9d66e2ae8568e79cebd
SHUBHRAJYOTI <shubhra24das@gmail.com>
2022-08-25 22:03:59 +0530

    second commit

---
+++ portfolio/src/components/header/CTA.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CTA = () => {
+    return (
+        <div className='cta'>
+            <a href="https://drive.google.com/file/d/1A-bgs3Es_I67HKMvnLF1sKzhiMwmC1vr/view?usp=sharing" className='btn'>Download CV</a>
+            <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+    )
+}
+
+export default CTA
